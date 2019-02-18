3f14b0dfe5a1bdde1e9dabad14e5ac90bafd855b
Michael Krasnitski <michael.krasnitski@gmail.com>
2019-02-18 15:14:51 -0500

    optimizations

--- network.py
+++ network.py
@@ -14,8 +14,6 @@ class Network:
 		self.sigmoid_derivs = [np.empty((n,)) for n in self.nodes]
 		self.clear()
 
-		self.size = len(self.nodes)
-
 	def save(self):
 		with open(self.path, 'wb') as f:
 			pickle.dump(self.nodes, f)
@@ -35,18 +33,13 @@ class Network:
 			return (x/(1 + abs(x))+1)/2
 		return 1/(2*(1+abs(x))**2)
 
-	# def sigmoid(self, x, deriv):
-	# 	if not deriv:
-	# 		return 1/(1+math.exp(-x))
-	# 	return math.exp(-x)/((1+math.exp(-x))**2)
-
 	def run(self, initial, output):
 		assert(len(initial) == self.nodes[0])
 		assert(len(output) == self.nodes[-1])
 		self.activations[0] = initial
-		self.y = output
+		self.y = np.array(output)
 
-		for l in range(1, self.size):
+		for l in range(1, len(self.nodes)):
 			for i in range(self.nodes[l]):
 				z = np.dot(self.activations[l-1], self.weights[l-1][:,i])
 				self.z[l][i] = z + self.biases[l-1][i]
@@ -56,12 +49,6 @@ class Network:
 		for a, y in zip(self.activations[-1], self.y):
 			cost += (a-y)**2
 		return cost
-
-	def gradCA(self):
-		C = [None]*(self.nodes[-1])
-		for i in range(self.nodes[-1]):
-			C[i] = 2*(self.activations[-1][i] - self.y[i])
-		return C
 		
 	def gradAWB(self, A_layer, WB_layer, p, q, t):
 		if A_layer == WB_layer + 1:
@@ -74,28 +61,26 @@ class Network:
 			wb = self.sigmoid_derivs[A_layer-1][q]
 			if t == 'w':
 				wb *= self.activations[WB_layer][p]
-			return wb*np.multiply(self.sigmoid_derivs[A_layer], self.weights[A_layer-1][q])
+			return wb*self.sigmoid_derivs[A_layer]*self.weights[A_layer-1][q]
 		else:
 			WB = self.gradAWB(A_layer-1, WB_layer, p, q, t)
 			sig = self.sigmoid_derivs[A_layer]
-			dot = np.dot(WB, self.weights[A_layer-1])
-			return sig*dot
+			return sig*np.dot(WB, self.weights[A_layer-1])
 
 	def single_descent(self):
-		C = self.gradCA()
+		C = 2*(self.activations[-1] - self.y)
 		L = len(self.nodes) - 1
 		for n in range(L):
+			BG = np.empty((self.nodes[n+1], self.nodes[-1]))
 			for q in range(self.nodes[n+1]):
+				WG = np.empty((self.nodes[n], self.nodes[-1]))
 				for p in range(self.nodes[n]):
-					# WG = self.gradAW(L, n, p, q)
-					WG = self.gradAWB(L, n, p, q, 'w')
-					self.gradW[n][p][q] += np.dot(C, WG)
-				# BG = self.gradAB(L, n, q)
-				BG = self.gradAWB(L, n, None, q, 'b')
-				self.gradB[n][q] += np.dot(C, BG)
+					WG[p] = self.gradAWB(L, n, p, q, 'w')
+				self.gradW[n][:,q] += np.dot(WG, C)
+				BG[q] = self.gradAWB(L, n, None, q, 'b')
+			self.gradB[n] += np.dot(BG, C)
 
 	def descend_batch(self, size):
-		C = self.gradCA()
 		L = len(self.nodes) - 1
 		for n in range(L):
 			self.weights[n] -= self.gradW[n]/size
--- train.py
+++ train.py
@@ -1,13 +1,11 @@
-import sys
+import argparse
 import math
 import pickle
 # import matplotlib.pyplot as plt
 import numpy as np
 from network import Network
+np.set_printoptions(suppress=True)
 
-num = 10
-if len(sys.argv) > 1:
-	num = int(sys.argv[1])
 n = Network('network')
 image = []
 labels = [0]*10
@@ -26,9 +24,19 @@ print()
 n.print_last()
 
 
-# batch_size = 100
-# num_batches = 1
-# epochs = 1
+# batch_size = 200
+# num_batches = 10
+# epochs = 20
+
+# images = [None]*batch_size*num_batches
+# labels = [None]*batch_size*num_batches
+# for i in range(batch_size*num_batches):
+# 	with open(f'image_data/data/{i:05d}', 'rb') as f:
+# 		images[i] = (pickle.load(f))
+# 		label = pickle.load(f)
+# 		labels[i] = [1 if j == label else 0 for j in range(10)]
+# print('unpickled')
+
 # for e in range(epochs):
 # 	print(f'Epoch {e}')
 # 	for batch in range(num_batches):
@@ -36,15 +44,10 @@ n.print_last()
 # 		try:
 # 			cost = 0
 # 			for b in range(batch_size):
-# 				image = []
-# 				labels = [0]*10
+# 				index = batch*batch_size + b
 # 				if b % 10 == 0:
-# 					print(f'{(batch*batch_size + b):03d}', end = ' ', flush=True)
-# 				with open(f'image_data/data/{(batch*batch_size + b):03d}', 'rb') as p:
-# 					image = pickle.load(p)
-# 					label = pickle.load(p)
-# 					labels[label] = 1
-# 				x = n.run(image, labels)
+# 					print(f'{index:05d}', end = ' ', flush=True)
+# 				x = n.run(images[index], labels[index])
 # 				cost += x
 # 				n.single_descent()
 # 		except KeyboardInterrupt:
